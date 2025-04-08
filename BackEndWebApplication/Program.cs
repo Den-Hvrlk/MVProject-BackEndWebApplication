2d95ef5dc6ce72c7b6383ac5803dfa084da541d7
DEN\megas <megassv10122003@gmail.com>
2025-04-08 18:57:39 +0300

    Changed policy for vercel frontend app

--- BackEndWebApplication/Program.cs
+++ BackEndWebApplication/Program.cs
@@ -1,20 +1,26 @@
 using BackEndWebApplication.Data;
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("https://mv-project-front-end-application.vercel.app")  // Разрешаем доступ с фронтенда
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
 });
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+                         ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")));
 
 builder.Services.AddControllers();
 
