fcd001b40ad346f3b35e3428cdf9d8ef8b1d1984
DEN\megas <megassv10122003@gmail.com>
2025-04-08 19:02:08 +0300

    updated cors policy

--- BackEndWebApplication/Program.cs
+++ BackEndWebApplication/Program.cs
@@ -2,25 +2,25 @@ using BackEndWebApplication.Data;
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
-Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://mv-project-front-end-application.vercel.app")  // Разрешаем доступ с фронтенда
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.WithOrigins("https://mv-project-front-end-application.vercel.app") // Разрешаем доступ с фронтенда
+              .AllowAnyHeader()  // Разрешаем все заголовки
+              .AllowAnyMethod()  // Разрешаем все HTTP методы
+              .AllowCredentials();  // Разрешаем отправку куки и авторизацию (если нужно)
     });
 });
 
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
-                         ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 
